d9b083b3c2d59fdad5817dba6485a06228d7ff38
HHG <95553132+heohgoo@users.noreply.github.com>
2023-07-12 11:07:31 +0900

    [level 2] Title: 피보나치 수, Time: 431.23 ms, Memory: 439 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,10 @@
+#dp
+def solution(n):
+    answer = 0
+    dp = [0]*100001
+    dp[0], dp[1] = 0, 1
+    
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    return dp[n]%1234567
