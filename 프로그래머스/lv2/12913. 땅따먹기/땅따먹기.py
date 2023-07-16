e3d703ee73d7f852dcff287290eeef7d1bafc21b
HHG <95553132+heohgoo@users.noreply.github.com>
2023-07-16 11:45:24 +0900

    [level 2] Title: 땅따먹기, Time: 199.70 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -20,13 +20,8 @@
 def solution(land):
     
     for i in range(1, len(land)):
-        for j in range(4):
-            tmp = land[i-1][j]
-            land[i-1][j] = -1
-            land[i][j] += max(land[i-1][0], land[i-1][1], land[i-1][2], land[i-1][3])
-            land[i-1][j] = tmp
-    
-    
-    
+        for j in range(len(land[0])):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
+        
     return max(land[-1])
     
