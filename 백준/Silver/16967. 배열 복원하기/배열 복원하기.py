c35eeded27a1d7f42507303c81d50877064bf248
HHG <95553132+heohgoo@users.noreply.github.com>
2023-08-11 17:58:11 +0900

    [Silver III] Title: 배열 복원하기, Time: 196 ms, Memory: 118884 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -1,23 +1,26 @@
-import sys
-input = sys.stdin.readline
 h, w, x, y = map(int, input().split())
-B  = []
-for i in range(h+x):
-	B.append(list(map(int, input().split())))
+grid = []
+tmp = [[0]*w for _ in range(h)]
 
-A = [[0]*(w+y) for _ in range(h+x)]
-for i in range(h+x):
-	for j in range(w+y):
-    		# 겹치지 않는 부분은 그대로 들어감 , 겹치는 부분은 B 배열에서 A 배배열의 x, y만큼 돌아가서 빼줌
-		if x <= i < x+h and y <= j < y+w:
-			A[i][j] = B[i][j] - A[i-x][j-y]
-		elif i < x or i >= x+h:
-			A[i][j] = B[i][j]
-		elif j < y or j >= y+w:
-			A[i][j] = B[i][j]
+
+for _ in range(h+x):
+    grid.append(list(map(int, input().split())))
 
 
 for i in range(h):
-	for j in range(w):
-		print(A[i][j], end = ' ')
-	print()
+    for j in range(w):
+        if x<=i<x+h and y<=j<y+w:
+            continue
+            # tmp[i][j] = grid[i+x][j+y] - grid[i][j]
+        else:
+            tmp[i][j] = grid[i][j]
+
+
+for i in range(h):
+    for j in range(w):
+        if x<=i<x+h and y<=j<y+w:
+            tmp[i][j] = grid[i][j] - tmp[i-x][j-y] 
+
+
+for t in tmp:
+    print(*t)
