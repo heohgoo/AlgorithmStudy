c1039567ea8aa6abd82bc3c6998f7fc5e0d9fdf0
HHG <95553132+heohgoo@users.noreply.github.com>
2023-08-23 10:31:22 +0900

    [Silver III] Title: 비슷한 단어, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,60 +1,20 @@
-#구현
+#배열로 간단하게, remove 이용
 n = int(input())
 word = list(input())
-word.sort()
-word_dict = {}
 answer = 0
 
-for w in word:
-    if w not in word_dict:
-        word_dict[w] = 1
-    else:
-        word_dict[w] += 1
-
-
 for _ in range(n-1):
+    cnt = 0
+    target = word[:]
     cp = list(input())
-    cp_dict = {}
-    cp.sort()
 
     for c in cp:
-        if c not in cp_dict:
-            cp_dict[c] = 1
+        if c in target:
+            target.remove(c)
         else:
-            cp_dict[c] += 1
+            cnt += 1
     
-    if cp_dict == word_dict: # 같은 구성
+    if len(target) <= 1 and cnt <= 1:
         answer += 1
-        continue
-
-    if set(cp) == set(word): # 같은 문자로 구성
-        if len(word)-1 <= len(cp) <= len(word)+1:
-            answer += 1
-            continue
-    
-    else:
-        # 사본 생성
-        tmp_word_dict = word_dict.copy()
-
-        for c in cp_dict:
-            if c in word_dict:
-                if tmp_word_dict[c] > cp_dict[c]:
-                    tmp_word_dict[c] -= cp_dict[c]
-                    cp_dict[c] = 0
-                else:
-                    cp_dict[c] -= tmp_word_dict[c]
-                    tmp_word_dict[c] = 0
-                
-        tmp_c, tmp_w = 0, 0
-
-        for c in cp_dict:
-            tmp_c += cp_dict[c]
-        
-        for w in tmp_word_dict:
-            tmp_w += tmp_word_dict[w]
-        
-        if tmp_c <= 1 and tmp_w <= 1:
-            answer += 1
-            continue
 
 print(answer)
